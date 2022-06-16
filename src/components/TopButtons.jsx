c20c4540ee9d2ec983e7c05fae63039638c39125
LvL090 <calvod879@gmail.com>
2022-06-16 14:49:59 +0200

    button update

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+
+function TopButtons() {
+
+const cities = [
+    {
+        id:1,
+        title: 'London',
+    },
+    {
+        id:2,
+        title: 'Sydney',
+    },
+    {
+        id:3,
+        title: 'Tokio',
+    },
+    {
+        id:4,
+        title: 'New York',
+    },
+    {
+        id:5,
+        title: 'Berlin',
+    },
+]
+    return (
+
+    <div className='flex items-center justify-around my-6'>
+            {cities.map((city) => (
+
+                <button key={city.id} className='text-white text-lg font-medium'>
+                    {city.title}
+                </button>
+
+            ))}
+        </div>
+    );
+}
+
+export default TopButtons;
